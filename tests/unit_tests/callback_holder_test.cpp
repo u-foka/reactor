efe33aa98cf6c5390a8cf9870442e2787468748c
Eisenberger Tamas <e.tamas@iwstudio.hu>
2020-01-04 16:25:09 +0100

    Tests fixed, and extended for callback holder

--- tests/unit_tests/callback_holder_test.cpp
+++ tests/unit_tests/callback_holder_test.cpp
@@ -7,29 +7,20 @@
 #include <reactor/callback_holder.hpp>
 
 namespace ph = std::placeholders;
+using ::testing::_;
+using ::testing::Invoke;
 
 struct callback_holder : public ::testing::Test
 {
-   class i_callback_test
+   struct mock_callback_test
    {
-    public:
-      virtual ~i_callback_test() {}
-
-      virtual void simple() = 0;
-      virtual void simple2() = 0;
-      virtual void simple3() = 0;
-      virtual void simple_arg(int arg) = 0;
-      virtual void complex_arg(const std::string &arg) = 0;
-   };
-
-   class mock_callback_test : public i_callback_test
-   {
-    public:
       MOCK_METHOD0(simple, void());
       MOCK_METHOD0(simple2, void());
       MOCK_METHOD0(simple3, void());
       MOCK_METHOD1(simple_arg, void(int));
       MOCK_METHOD1(complex_arg, void(const std::string &));
+      MOCK_METHOD1(moved_arg, void(std::unique_ptr<std::string> &&));
+      MOCK_METHOD3(multiple_args, void(int, double, std::string));
    };
 
    mock_callback_test mock;
@@ -39,13 +30,17 @@ struct callback_holder : public ::testing::Test
    std::function<void()> simple3_cb;
    std::function<void(int)> simple_arg_cb;
    std::function<void(const std::string &arg)> complex_arg_cb;
+   std::function<void(std::unique_ptr<std::string> &&)> moved_arg_cb;
+   std::function<void(int, double, std::string)> multiple_args_cb;
 
    callback_holder()
-         : simple_cb(std::bind(&i_callback_test::simple, &mock))
-         , simple2_cb(std::bind(&i_callback_test::simple2, &mock))
-         , simple3_cb(std::bind(&i_callback_test::simple3, &mock))
-         , simple_arg_cb(std::bind(&i_callback_test::simple_arg, &mock, ph::_1))
-         , complex_arg_cb(std::bind(&i_callback_test::complex_arg, &mock, ph::_1))
+         : simple_cb(std::bind(&mock_callback_test::simple, &mock))
+         , simple2_cb(std::bind(&mock_callback_test::simple2, &mock))
+         , simple3_cb(std::bind(&mock_callback_test::simple3, &mock))
+         , simple_arg_cb(std::bind(&mock_callback_test::simple_arg, &mock, ph::_1))
+         , complex_arg_cb(std::bind(&mock_callback_test::complex_arg, &mock, ph::_1))
+         , moved_arg_cb(std::bind(&mock_callback_test::moved_arg, &mock, ph::_1))
+         , multiple_args_cb(std::bind(&mock_callback_test::multiple_args, &mock, ph::_1, ph::_2, ph::_3))
    {
    }
 };
@@ -150,4 +145,36 @@ TEST_F(callback_holder, disconnect)
    ASSERT_TRUE(cb.disconnect(cb_id3));
 
    cb();
+}
+
+TEST_F(callback_holder, forward_correct)
+{
+   const std::string txt("here I am");
+   auto to_be_moved = std::make_unique<std::string>(txt);
+   std::string callback_arg;
+   EXPECT_CALL(mock, moved_arg(_)).WillRepeatedly(Invoke([&](std::unique_ptr<std::string> &&arg){
+      std::unique_ptr<std::string> moved(std::move(arg));
+      callback_arg = *moved;
+   }));
+   
+   iws::callback_holder<std::unique_ptr<std::string> &&> cb;
+   cb.connect(moved_arg_cb);
+
+   cb(std::move(to_be_moved));
+   EXPECT_EQ(to_be_moved.get(), nullptr); // Should be empty as it was moved into the callback
+   EXPECT_EQ(callback_arg, txt);
+
+   to_be_moved = std::make_unique<std::string>(txt); // Re-initialize the pointer
+   cb.connect(moved_arg_cb); // Connect the callback second time
+
+#if defined (__unix__) || (defined (__APPLE__) && defined (__MACH__))
+   EXPECT_EXIT((cb(std::move(to_be_moved)),exit(0)),::testing::KilledBySignal(SIGSEGV),".*");
+#else
+   EXPECT_DEATH_IF_SUPPORTED((cb(std::move(to_be_moved)),exit(0)),".*");
+#endif
+}
+
+TEST_F(callback_holder, multiple_args)
+{
+   // TODO
 }
